80746b6b5505096725e89de303c817089b859c19
gabriel <gabriel@pop-os.meuintelbras.local>
2023-12-04 00:20:09 -0300

    criar tela de registrar usuario

---
+++ src/pages/sign_up/index.tsx
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import './index.css';
+import PasswordInput from '../../components/password_input';
+
+const SignUpPage: React.FC = () => {
+  const [email, setEmail] = useState<string | undefined>();
+  const [password, setPassword] = useState<string | undefined>();
+  const [emailError, setEmailError] = useState<string | undefined>();
+  const [passwordError, setPasswordError] = useState<string | undefined>();
+  const [selectedOption, setSelectedOption] = useState('admin');
+
+  const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const handleSignUp = () => {
+    console.log('Email: ', email);
+    console.log('Senha: ', password);
+    console.log('Tipo de usuário: ', selectedOption);
+  };
+
+  const validateEmail = (input: string): void => {
+    // Expressão regular para validar o formato do e-mail
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(input)) {
+      setEmail(undefined);
+      setEmailError('Por favor, insira um e-mail válido');
+    } else {
+      setEmail(input);
+      setEmailError('');
+    }
+  };
+
+  const validatePassword = (input: string): void => {
+    if (input?.length < 8) {
+      setPassword(undefined);
+      setPasswordError('Senha deve ter mais de 8 dígitos');
+    } else {
+      setPassword(input);
+      setPasswordError('');
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <div className="login-container">
+        <h2>Registrar novo usuário</h2>
+        <form>
+          <label htmlFor="email">E-mail:</label>
+          <input
+            placeholder="email@email.com"
+            type="email"
+            id="email"
+            onChange={(e) => {
+              // valida a cada caractere digitado se o email é valido
+              validateEmail(e.target.value);
+            }}
+          />
+          {emailError && <p className="error-message">{emailError}</p>}
+
+          <label htmlFor="password">Senha:</label>
+          <div>
+            <PasswordInput
+              onChange={(e) => {
+                // valida a cada caractere digitado se a senha é valida
+                validatePassword(e.target.value);
+              }}
+            />
+            {passwordError && <p className="error-message">{passwordError}</p>}
+          </div>
+
+          <div className="radio-buttons">
+            <label>
+              <input
+                type="radio"
+                value="admin"
+                checked={selectedOption === 'admin'}
+                onChange={handleOptionChange}
+              />
+              Administrador
+            </label>
+
+            <label>
+              <input
+                type="radio"
+                value="client"
+                checked={selectedOption === 'client'}
+                onChange={handleOptionChange}
+              />
+              Cliente
+            </label>
+          </div>
+
+          <button
+            type="button"
+            onClick={handleSignUp}
+            disabled={!email || !password}
+            className={!email || !password ? 'disable-button' : undefined}
+          >
+            Criar Conta
+          </button>
+
+          <div className="divider" />
+
+          <a href="/login">Voltar para login</a>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
